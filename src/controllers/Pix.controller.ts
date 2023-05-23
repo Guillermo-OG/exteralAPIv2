bca613bfdbd501e3197f9aad45c4b4f68374a837
Matheus Rafalski <matheus.rafalski@ghbranding.com.br>
2023-05-23 17:09:09 -0300

    feat: error when no document specified

--- src/controllers/Pix.controller.ts
+++ src/controllers/Pix.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { PixRepository } from '../repository/Pix.repository'
+import { ValidationError } from 'yup'
 
 export class PixController {
     public async listByDocument(req: Request, res: Response, next: NextFunction) {
@@ -7,8 +8,11 @@ export class PixController {
             const document = req.query.document
             const keyType = req.query.pixKeyType
 
-            const pixRepository = PixRepository.getInstance()
+            if(!document) {
+                throw new ValidationError('No document specified')
+            }
 
+            const pixRepository = PixRepository.getInstance()
             const pixKeys = await pixRepository.listByDocument(document as string, keyType as string)
 
             res.json(pixKeys)
