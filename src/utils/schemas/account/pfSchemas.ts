f8b07a349405b273c87a0ebd37d81377e974e7c3
Matheus Rafalski <matheus.rafalski@ghbranding.com.br>
2023-05-26 18:25:39 -0300

    feat: receiving the necessary fields on payload

--- src/utils/schemas/account/pfSchemas.ts
+++ src/utils/schemas/account/pfSchemas.ts
@@ -1,84 +1,50 @@
 import * as yup from 'yup'
-import { unMask } from '../../masks'
-import { isEmailValid, isPhoneValid, validateCNPJ, validateCPF } from '../../validations'
+import { isEmailValid, validateCPF } from '../../validations'
 import { isValid } from 'date-fns'
 import addressSchema from './addressSchema'
 
 export const PfCreateSchema = new yup.ObjectSchema({
     callbackURL: yup.string().required(),
-    cpfCnpj: yup
-        .string()
-        .required()
-        .test(value => {
-            const raw = unMask(value)
-            if (raw.length > 11) {
-                return validateCNPJ(raw)
-            }
-            return validateCPF(raw)
-        }),
-    email: yup
-        .string()
-        .required()
-        .test(value => {
-            return isEmailValid(value)
-        }),
-    dataAbertura: yup
-        .string()
-        .required()
-        .test(value => {
-            return isValid(new Date(value))
-        }),
-    celular: yup
-        .string()
-        .test(value => {
-            if (value) {
-                return isPhoneValid(value)
-            }
-            return true
-        })
-        .nullable(),
-    razaoSocial: yup.string().required(),
-    usuarios: yup.array().of(
-        yup.object().shape({
-            email: yup
-                .string()
-                .required()
-                .test(value => {
-                    return isEmailValid(value)
-                }),
-            nome: yup.string().required(),
-            pep: yup.boolean().required(),
-            dataNascimento: yup
-                .string()
-                .required()
-                .test(value => {
-                    return isValid(new Date(value))
-                }),
-            cpf: yup
-                .string()
-                .required()
-                .test(value => {
-                    return validateCPF(value)
-                }),
-            celular: yup.string().test(value => {
-                if (value) {
-                    return isPhoneValid(value)
-                }
-                return true
+    account_owner: yup.object().shape({
+        address: addressSchema,
+        email: yup
+            .string()
+            .required()
+            .test(value => {
+                return isEmailValid(value)
             }),
-            conta: yup.object().shape({
-                subConta: yup.object().shape({
-                    cnae: yup.string().required(),
-                    nomeFantasia: yup.string().required(),
-                    nomeMae: yup.string().required(),
-                    nacionalidade: yup.string().required(),
-                    rgNumero: yup
-                        .string()
-                        .required()
-                        .matches(/^\d{9}$/),
-                }),
+        person_type: yup.string().required().oneOf(['legal']),
+        phone: yup.object().shape({
+            area_code: yup
+                .string()
+                .required()
+                .matches(/^\d{2}$/),
+            country_code: yup
+                .string()
+                .required()
+                .matches(/^\d{2}$/),
+            number: yup
+                .string()
+                .required()
+                .matches(/^\d{9}$/),
+        }),
+        individual_document_number: yup
+            .string()
+            .required()
+            .test(value => {
+                return validateCPF(value)
             }),
-        })
-    ),
-    endereco: addressSchema,
+        document_identification: yup.string().required(),
+        name: yup.string().required(),
+        is_pep: yup.boolean().required(),
+        mother_name: yup.string().required(),
+        nationality: yup.string().required(),
+        proof_of_residence: yup.string().required(),
+        birth_date: yup
+            .string()
+            .required()
+            .test(value => {
+                return isValid(new Date(value))
+            }),
+    }),
 })
