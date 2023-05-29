3168ec3f1648e492b813aad40027a75cdb4b789f
Matheus Rafalski <matheus.rafalski@ghbranding.com.br>
2023-05-29 09:50:50 -0300

    fix: ensure allowed user exists

--- src/utils/schemas/account/createAccountSchema.ts
+++ src/utils/schemas/account/createAccountSchema.ts
@@ -4,7 +4,7 @@ import { PfCreateSchema } from './pfSchemas'
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export const CreateAccountSchema: any = yup.lazy(value => {
-    if ('allowed_user' in value) {
+    if ('allowed_user' in value && !!value.allowed_user) {
         return PjCreateSchema
     } else {
         return PfCreateSchema
