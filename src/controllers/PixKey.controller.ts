a69659e780e68507895057a6d816a9b2c6b52817
Guillermo <guillermo@villelabrasil.com>
2023-08-22 18:00:18 -0300

    routetoPixKeyCrate

--- src/controllers/PixKey.controller.ts
+++ src/controllers/PixKey.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import { ValidationError } from 'yup'
 import { PixKeyRepository } from '../repository'
+import { QiTechTypes } from '../infra'
+import { QiTechService } from '../services'
 
 export class PixKeyController {
     public async listByDocument(req: Request, res: Response, next: NextFunction) {
@@ -16,6 +18,20 @@ export class PixKeyController {
             const pixKeys = await pixRepository.listByDocument(document as string, keyType as string)
 
             res.json(pixKeys)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    public async createPixKey(req: Request, res: Response, next: NextFunction) {
+        try {
+            const qiTechService = QiTechService.getInstance()
+
+            const payload: QiTechTypes.Pix.ICreatePix = req.body // A partir do corpo da solicitação
+
+            const pixKey = await qiTechService.createPixKey(payload)
+
+            res.json(pixKey)
         } catch (error) {
             next(error)
         }
