b7e04ff155c6bcc215fe2a146257a674fc9d5260
Guillermo <guillermo@villelabrasil.com>
2023-10-03 16:19:04 -0300

    pixRequestFix

---
+++ src/repository/PixLimitsRequest.repository.ts
@@ -0,0 +1,22 @@
+﻿import { HydratedDocument } from 'mongoose'
+import { PixLimitsRequest, IPixLimitsRequest } from '../models'
+
+export class PixLimitsRequestRepository {
+    private static instance: PixLimitsRequestRepository
+
+    public static getInstance(): PixLimitsRequestRepository {
+        if (!PixLimitsRequestRepository.instance) {
+            PixLimitsRequestRepository.instance = new PixLimitsRequestRepository()
+        }
+        return PixLimitsRequestRepository.instance
+    }
+
+    public async create(data: IPixLimitsRequest): Promise<HydratedDocument<IPixLimitsRequest>> {
+        const newBillingConfiguration = new PixLimitsRequest(data)
+        return await newBillingConfiguration.save()
+    }
+
+    public async update(document: string, data: Partial<IPixLimitsRequest>): Promise<void> {
+        await PixLimitsRequest.updateOne({ document }, { $set: data })
+    }
+}
