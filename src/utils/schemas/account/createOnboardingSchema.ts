dbf160f4c6c533cf9e77f587505885180c1511ff
Guillermo <guillermo@villelabrasil.com>
2023-11-30 17:05:37 -0300

    addPFOBvalidations

--- src/utils/schemas/account/createOnboardingSchema.ts
+++ src/utils/schemas/account/createOnboardingSchema.ts
@@ -1,12 +1,12 @@
 import * as yup from 'yup'
-import { PfCreateSchema } from './pfSchemas'
 import { OnboardingPJSchema } from './onboardingPJSchemas'
+import { OnboardingPFSchema } from './onboardingPFSchemas'
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export const CreateOnboardingSchema: any = yup.lazy(value => {
     if ('allowed_user' in value && !!value.allowed_user) {
         return OnboardingPJSchema
     } else {
-        return PfCreateSchema
+        return OnboardingPFSchema
     }
 })
---
+++ src/utils/schemas/account/onboardingPFSchemas.ts
@@ -0,0 +1,50 @@
+import * as yup from 'yup'
+import { isEmailValid, validateCPF } from '../../validations'
+import { isValid } from 'date-fns'
+import addressSchema from './addressSchema'
+
+export const OnboardingPFSchema = new yup.ObjectSchema({
+    callbackURL: yup.string().required('URL de retorno é obrigatório'),
+    account_owner: yup.object().shape({
+        address: addressSchema,
+        email: yup
+            .string()
+            .required('E-mail é obrigatório')
+            .test('Validação de E-mail', 'E-mail inválido', value => {
+                return isEmailValid(value)
+            }),
+        person_type: yup.string().required('Tipo de pessoa é obrigatório').oneOf(['natural'], 'Tipo de pessoa deve ser "natural"'),
+        phone: yup.object().shape({
+            area_code: yup
+                .string()
+                .required('DDD é obrigatório')
+                .matches(/^\d{2}$/, 'DDD deve ter dois dígitos'),
+            country_code: yup
+                .string()
+                .required('Código do país é obrigatório')
+                .matches(/^\d{2}$/, 'Código do país deve ter dois dígitos'),
+            number: yup
+                .string()
+                .required('Número de telefone é obrigatório')
+                .matches(/^\d{9}$/, 'Número de telefone deve ter 9 dígitos'),
+        }),
+        individual_document_number: yup
+            .string()
+            .required('Número do documento individual é obrigatório')
+            .test('Validação de CPF', 'CPF inválido', value => {
+                return validateCPF(value)
+            }),
+        document_identification: yup.string().nullable().notRequired(),
+        name: yup.string().required('Nome é obrigatório'),
+        is_pep: yup.boolean().required('Campo is_pep é obrigatório'),
+        mother_name: yup.string().required('Nome da mãe é obrigatório'),
+        nationality: yup.string().required('Nacionalidade é obrigatória'),
+        proof_of_residence: yup.string().nullable().notRequired(),
+        birth_date: yup
+            .string()
+            .required('Data de nascimento é obrigatória')
+            .test('Validação de data', 'Data inválida', value => {
+                return isValid(new Date(value))
+            }),
+    }),
+})
