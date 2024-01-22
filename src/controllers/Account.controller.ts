988ce469b02edd0aa4fb3b65e386aa652985c767
Guillermo <guillermo@villelabrasil.com>
2024-01-22 17:34:13 -0300

    contas direto da qitech/desativar pooling

--- src/controllers/Account.controller.ts
+++ src/controllers/Account.controller.ts
@@ -35,6 +35,21 @@ export class AccountController {
         }
     }
 
+    public async listQiTechByDocument(req: Request, res: Response, next: NextFunction): Promise<void> {
+        const qiTechService = QiTechService.getInstance()
+        try {
+            const page = Number(req.query.page) || 1
+            const pageSize = Number(req.query.pageSize) || 100
+            const document = req.params.document
+    
+            const accounts = await qiTechService.listAccountsQiTech(document, page, pageSize)
+    
+            res.status(200).json(accounts)
+        } catch (error) {
+            next(await qiTechService.decodeError(error))
+        }
+    }
+
     public async List(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const page = Number(req.query.page ?? '1')
